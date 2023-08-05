daed130df1d0e9a0815cb6a0c185b9f35c79a457
Sudip Chowdhury <rexalex826@gmail.com>
2023-08-05 20:49:08 +0530

    set 1 problems

---
+++ set-1/Problems.py
@@ -0,0 +1,54 @@
+print("Hello,World")
+# string
+_a= "sudip"
+#float
+b=3.14
+#int
+c=23
+#boolean
+d=True
+#list
+e=["rajesh",12,12.32,False]
+#tuple
+f = (10, 20, 30, 'world', False)
+#dictionary
+_dict = {'name': 'sudip', 'age': 23, 'city': 'Kolkata'}
+
+print(type(_a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(_dict))
+
+my_list=list(range(1, 11))
+print(my_list)
+
+number_to_add=3
+my_list.append(number_to_add)
+print(my_list)
+
+number_to_remove=6
+my_list.remove(number_to_remove)
+print(my_list)
+
+my_list.sort()
+print(my_list)
+
+num_list=[10,20,25,87]
+sum=0
+list_len=len(num_list)
+for number in num_list:
+  sum+=number
+
+print(sum)
+print(sum/list_len)
+
+def new_string(input_string):
+    reversed_string = ""
+    for char in input_string:
+        reversed_string = char + reversed_string
+    return reversed_string
+
+print(new_string("sudip"))
