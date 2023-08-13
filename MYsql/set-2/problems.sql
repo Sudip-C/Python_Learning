497d33d38edfd10644ad5aa37460b76f2ac274d0
Sudip Chowdhury <rexalex826@gmail.com>
2023-08-13 12:24:47 +0530

    set2 question 3 done

--- MYsql/set-2/problems.sql
+++ MYsql/set-2/problems.sql
@@ -14,3 +14,6 @@ INSERT INTO Restaurants VALUES (2, 'Arnold', 'Continental', 'Mumbai' , 4.1,false
 INSERT INTO Restaurants VALUES (4, 'Johns', 'Chinese', 'Kolkata' , 4.2,true);
 INSERT INTO Restaurants VALUES (3, 'Bonny', 'Korean', 'Bangalore' , 4.3,true);
 INSERT INTO Restaurants VALUES (5, 'Ateve', 'Japanese', 'Mumbai' , 4.7,false);
+
+--problem 3
+ SELECT * FROM Restaurants ORDER BY average_rating DESC
