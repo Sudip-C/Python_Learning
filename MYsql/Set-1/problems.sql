c1ee5b70f41f02e46e05c4f5a8e629a25f04e106
Sudip Chowdhury <rexalex826@gmail.com>
2023-08-13 11:50:30 +0530

    problem 10 done

---
+++ MYsql/Set-1/problems.sql
@@ -0,0 +1,41 @@
+--problem 1
+
+CREATE TABLE Customers (
+  id INTEGER PRIMARY KEY,
+  name TEXT NOT NULL,
+  email TEXT NOT NULL,
+  address TEXT NOT NULL,
+  phone TEXT NOT NULL
+)
+
+--problem 2
+INSERT INTO Customers VALUES (1, 'Ryan', 'M@gmail.com', 'Kolkata' , '6291847283');
+INSERT INTO Customers VALUES (2, 'Arnold', 'M@gmail.com', 'Kolkata' , '6291847283');
+INSERT INTO Customers VALUES (4, 'John', 'M@gmail.com', 'Kolkata' , '6291847283');
+INSERT INTO Customers VALUES (3, 'Wick', 'M@gmail.com', 'Kolkata' , '6291847283');
+INSERT INTO Customers VALUES (5, 'Ateve', 'M@gmail.com', 'Kolkata' , '6291847283');
+--problem 3
+SELECT * FROM Customers 
+
+--problem 4
+SELECT name, email FROM Customers 
+
+--problem 5
+SELECT * FROM Customers WHERE id=3
+
+--problem 6
+SELECT * FROM Customers WHERE name LIKE 'A%';
+\
+--problem 7
+SELECT * FROM Customers ORDER BY name DESC;
+
+--problem 8
+UPDATE Customers SET address ='Barasat' WHERE id = 4;
+
+--problems 9
+SELECT * FROM Customers
+ORDER BY id ASC
+LIMIT 3;
+
+--problem 10
+DELETE  FROM Customers WHERE id=2;
