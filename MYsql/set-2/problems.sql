f3303d44473ff8bd5be30de2fee619b1145b7086
Sudip Chowdhury <rexalex826@gmail.com>
2023-08-13 12:21:49 +0530

    set2 question 2 done

--- MYsql/set-2/problems.sql
+++ MYsql/set-2/problems.sql
@@ -9,3 +9,8 @@ CREATE TABLE Restaurants (
 );
 
 --problem 2
+INSERT INTO Restaurants VALUES (1, 'Ryan', 'Indian', 'Kolkata' , 4.9,true);
+INSERT INTO Restaurants VALUES (2, 'Arnold', 'Continental', 'Mumbai' , 4.1,false);
+INSERT INTO Restaurants VALUES (4, 'Johns', 'Chinese', 'Kolkata' , 4.2,true);
+INSERT INTO Restaurants VALUES (3, 'Bonny', 'Korean', 'Bangalore' , 4.3,true);
+INSERT INTO Restaurants VALUES (5, 'Ateve', 'Japanese', 'Mumbai' , 4.7,false);
