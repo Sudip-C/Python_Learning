e7c19f12613486b121a851f376723a649a7bcde0
Sudip Chowdhury <rexalex826@gmail.com>
2023-08-13 12:17:45 +0530

    set2 question 1 done

---
+++ MYsql/set-2/problems.sql
@@ -0,0 +1,11 @@
+--problem 1
+CREATE TABLE Restaurants (
+    id INT PRIMARY KEY,
+    name VARCHAR(100),
+    cuisine_type VARCHAR(100),
+    location VARCHAR(255),
+    average_rating DECIMAL(3,2),
+    delivery_available BOOLEAN
+);
+
+--problem 2
